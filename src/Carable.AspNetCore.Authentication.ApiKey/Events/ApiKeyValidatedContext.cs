0b5d01f488a5c8cd9f6aec103811b36555a9ba7b
Oskar Gewalli <oskar@gewalli.se>
2017-09-15 15:02:06 +0200

    Upgrading to netcore2

--- src/Carable.AspNetCore.Authentication.ApiKey/Events/ApiKeyValidatedContext.cs
+++ src/Carable.AspNetCore.Authentication.ApiKey/Events/ApiKeyValidatedContext.cs
@@ -1,14 +1,18 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
 
 namespace Carable.AspNetCore.Authentication.ApiKey
 {
-    public class ApiKeyValidatedContext : BaseApiKeyContext
+    public class ApiKeyValidatedContext : ResultContext<ApiKeyOptions>
     {
-        public ApiKeyValidatedContext(HttpContext context, ApiKeyOptions options)
-            : base(context, options)
+        public ApiKeyValidatedContext(
+            HttpContext context, 
+            AuthenticationScheme scheme,
+            ApiKeyOptions options)
+            : base(context, scheme, options)
         {
         }
 
